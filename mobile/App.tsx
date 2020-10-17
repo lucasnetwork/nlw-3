bc336894e7493294ff529d0b94ea39989f14a385
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-10-17 15:15:19 -0300

    add routes mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar'
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {View ,Text} from 'react-native';
 
+import {useFonts} from 'expo-font'
+import {Nunito_600SemiBold,Nunito_700Bold,Nunito_800ExtraBold} from '@expo-google-fonts/nunito'
+import Routes from './src/routes'
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold
+  })
+
+  if(!fontsLoaded){
+    return (<View><Text>carregando</Text></View>);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <>
+      <Routes/>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
